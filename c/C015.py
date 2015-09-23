e30a537e006b48cf13e718a0ef86ac7edc3cf251
shinespark <shinespark64@gmail.com>
2015-09-24 01:55:19 +0900

    update c

--- c/C015.py
+++ c/C015.py
@@ -1,23 +1,31 @@
 # coding: utf-8
 import sys
 
-lines = [x.strip() for x in sys.stdin.readlines()]
 
-max_num = int(lines[0])
-receipt_lines = lines[1: 1 + max_num]
+def main():
+    lines = [x.strip() for x in sys.stdin.readlines()]
+    #  dummy = '''3
+#  1 1024
+#  11 2048
+#  21 4196
+#  '''
+    #  lines = [x.strip('\n') for x in dummy.split('\n')]
 
-# receiptごとに処理
-points = 0
-for receipt_line in receipt_lines:
-    splited_receipt_line = receipt_line.split()
-    date = splited_receipt_line[0]
-    value = int(splited_receipt_line[1])
+    receipt_lines = lines[1: 1 + int(lines[0])]
 
-    if date.find('3') >= 0:
-        points += int(value * 0.03)
-    elif date.find('5') >= 0:
-        points += int(value * 0.05)
-    else:
-        points += int(value * 0.01)
+    point = 0
+    for line in receipt_lines:
+        day, money = [int(x) for x in line.split()]
+        day = str(day)
 
-print points
+        if '3' in day:
+            point += int(money * 0.03)
+        elif '5' in day:
+            point += int(money * 0.05)
+        else:
+            point += int(money * 0.01)
+
+    print point
+
+if __name__ == "__main__":
+    main()
